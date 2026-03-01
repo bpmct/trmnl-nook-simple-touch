e1087e23a56e4e3a483f8231554829208b6a5c16
Ben Potter <me@bpmct.net>
2026-03-01 12:21:50 -0600

    gift mode: increase top padding for breathing room

--- src/com/bpmct/trmnl_nook_simple_touch/DisplayActivity.java
+++ src/com/bpmct/trmnl_nook_simple_touch/DisplayActivity.java
@@ -878,7 +878,7 @@ public class DisplayActivity extends Activity {
         
         LinearLayout giftLayout = new LinearLayout(this);
         giftLayout.setOrientation(LinearLayout.VERTICAL);
-        giftLayout.setPadding(50, 30, 50, 40);
+        giftLayout.setPadding(50, 60, 50, 40);
         giftLayout.setLayoutParams(scrollParams);
         
         // Logo + Title row
